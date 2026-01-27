a302f5e97f2047c0fb99af8d3e765e70ec6d0d64
andry81 <andry@inbox.ru>
2026-01-27 17:00:55 +0300

    2026.01.27: * changed: scripts/bat/abort.bat: code improvement * refactor: scripts/bat/abort.bat: code refactor

--- scripts/bat/abort.bat
+++ scripts/bat/abort.bat
@@ -1,5 +1,4 @@
-@echo off
-(call)
+@echo off & (call)
 (
   setlocal DISABLEDELAYEDEXPANSION
   if 0 "%~nx0: %~1"
@@ -11,22 +10,18 @@ rem USAGE:
 rem   abort.bat [<message>]
 
 rem Description:
-rem   Tests `<message>` and if is not empty, then stops an outer script
-rem   execution with the error message:
+rem   Stops an outer script execution with the error message:
 rem     `"abort.bat: <message>" was unexpected at this time.`
-rem   If `<message>` is empty, then stops an outer script execution
-rem   with the error message:
-rem     `The syntax of the command is incorrect.`
 rem   Sets not zero error level before the abort.
 rem   The `cmd.exe /c exit -1073741510` command emits CTRL-BREAK just in case
 rem   of execution continue.
 
 rem Examples:
 rem   1. >abort.bat
-rem      The syntax of the command is incorrect.
+rem      "abort.bat: " was unexpected at this time.
 rem
 rem   2. >abort.bat a b c
-rem      a was unexpected at this time.
+rem      "abort.bat: a" was unexpected at this time.
 rem
 rem   3. >abort.bat "a b c"
-rem      "a b c" was unexpected at this time.
+rem      "abort.bat: a b c" was unexpected at this time.
