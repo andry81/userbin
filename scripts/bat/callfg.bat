8ac4f56cd5ae894e1f2ceea58da2b0e314f6499f
andry81 <andry@inbox.ru>
2026-04-29 11:53:56 +0300

    2026.04.29: * new: scripts/bat/callf*.bat: wrapper scripts to call `callf*.exe` utilities from `contools--utils` project

---
+++ scripts/bat/callfg.bat
@@ -0,0 +1,8 @@
+@echo off
+
+if not defined PROJECTS_ROOT "%~dp0abort.bat" "PROJECTS_ROOT environment variable is not defined"
+
+"%PROJECTS_ROOT%/andry81/contools/contools--utils/bin/contools/callfg.exe" %*
+
+rem USAGE:
+rem   <See `src/callf/help.tpl` for details>
