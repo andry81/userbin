99a9d747abce080fc38e3aa2e1cae0983194d9a2
andry81 <andry@inbox.ru>
2026-05-10 21:00:02 +0300

    2026.05.10: * new: scripts/bat/reset-font-file-permissions.bat: wrapper script to call `reset_font_file_permissions.bat` script from `contools--admin` project

---
+++ scripts/bat/reset-font-file-permissions.bat
@@ -0,0 +1,26 @@
+@echo off
+
+if not defined PROJECTS_ROOT "%~dp0abort.bat" "PROJECTS_ROOT environment variable is not defined"
+
+"%PROJECTS_ROOT%/andry81/contools/contools--admin/scripts/Windows/Fonts/reset_font_file_permissions.bat" %*
+
+rem USAGE:
+rem   update_file_permissions.bat [<flags>] [--] <glob-files>...
+
+rem Description:
+rem   Script to update a font file permissions before or after install into
+rem   system fonts directory.
+
+rem <flags>:
+rem   -WD
+rem     Working directory path used instead of current directory path.
+rem     Has no effect if <glob-files> is absolute path.
+rem
+rem   -nolog
+rem     Disable logging and so a log directory allocation.
+
+rem --:
+rem   Separator to stop parse flags.
+
+rem <glob-files>
+rem   File path list with globbing.
